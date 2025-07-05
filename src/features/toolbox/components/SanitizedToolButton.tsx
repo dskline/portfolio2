115eec0ca0ef6dfc1a832d9e6c751dd5fa870e63
dskline <dskline1@gmail.com>
2025-07-04 20:00:53 -0400

    code review and undo delete

--- src/features/toolbox/components/SanitizedToolButton.tsx
+++ src/features/toolbox/components/SanitizedToolButton.tsx
@@ -1,12 +1,18 @@
-import DOMPurify from "dompurify";
-import { JSDOM } from "jsdom";
 import { UnsanitizedToolButton } from "@/features/toolbox/components/UnsanitizedToolButton";
 import type { Tool } from "@/features/toolbox/schema";
 
-const purify =
-  typeof window === "undefined" ? DOMPurify(new JSDOM("").window) : DOMPurify;
-
-export function SanitizedToolButton(tool: Tool) {
+async function getPurify() {
+  if (typeof window === "undefined") {
+    const { default: DOMPurify } = await import("dompurify");
+    const { JSDOM } = await import("jsdom");
+    return DOMPurify(new JSDOM("").window);
+  } else {
+    const { default: DOMPurify } = await import("dompurify");
+    return DOMPurify;
+  }
+}
+export async function SanitizedToolButton(tool: Tool) {
+  const purify = await getPurify();
   // Sanitize the SVG logo to prevent XSS attacks
   const sanitizedLogo = purify.sanitize(tool.logo, {
     // allow all safe SVG elements and SVG Filters, no HTML or MathML
